de9bf8bc20baa6cc7b8ae3f0a89b0e154931d471
zjslxzhing795 <1027502235@qq.com>
2022-12-16 18:27:56 +0800

    feat(notification): 支持全局config配置

--- packages/Notification/notification.tsx
+++ packages/Notification/notification.tsx
@@ -17,8 +17,16 @@ interface config {
   btn?: React.ReactNode; // 自定义关闭按钮
   icon?: React.ReactNode; // 自定义图标
   type?: 'info' | 'success' | 'warning' | 'error';
+  isGlobal?: boolean;
 }
 export interface NotificationProps extends config {}
+interface globalProps {
+  duration?: number | null; // null 或 0时不关闭
+  placement?: 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
+  bottom?: number; // 消息从底部弹出时，距离底部的位置，单位像素
+  top?: number; // 消息从顶部弹出时，距离顶部的位置，单位像素
+  closeIcon?: React.ReactNode; // 自定义关闭图标
+}
 type func = (config: NotificationProps) => void;
 interface apiProps {
   open: func;
@@ -29,6 +37,7 @@ interface apiProps {
 }
 type contextHolderProps = string;
 interface notificationProps extends apiProps {
+  config: func;
   useNotification: () => [apiProps, contextHolderProps];
 }
 
@@ -38,25 +47,27 @@ el.className = 'mzl_notification';
 wrapper.className = 'mzl_notification_wrapper';
 el.append(wrapper);
 
+var globalParams: globalProps = {};
+
 function Notification(props: NotificationProps): JSX.Element {
   const {
     message,
     description,
     className = '',
     style = {},
-    placement = 'topLeft',
+    placement,
     type = '',
-    bottom = 24,
-    top = 24,
-    closeIcon = <i className="m-icon-close" />,
+    bottom,
+    top,
+    closeIcon,
     btn,
     icon = null,
-    duration = 4.5,
+    duration,
     onClick,
     onClose,
+    isGlobal = false,
   } = props;
-
-  const [timer, setTimer] = useState<number | null>(null);
+  let timer: number | null = null;
 
   const icons = {
     info: 'm-icon-prompt-filling',
@@ -64,12 +75,27 @@ function Notification(props: NotificationProps): JSX.Element {
     warning: 'm-icon-warning-filling',
     error: 'm-icon-delete-filling',
   };
+
+  const getTime = () => {
+    if (duration === 0 || duration === null || duration) return duration;
+    if (
+      globalParams.duration === 0 ||
+      globalParams.duration === null ||
+      globalParams.duration
+    )
+      return globalParams.duration;
+    return 4.5;
+  };
+
   useEffect(() => {
-    if (duration) {
-      let t = setTimeout(() => {
+    if (isGlobal) {
+      globalParams = { bottom, closeIcon, duration, placement, top };
+    }
+    let time = getTime();
+    if (time) {
+      timer = setTimeout(() => {
         document.body.removeChild(el);
-      }, duration * 1000);
-      setTimer(t);
+      }, time * 1000);
     }
   }, []);
 
@@ -85,46 +111,73 @@ function Notification(props: NotificationProps): JSX.Element {
   const onClickNotification = () => {
     onClick && onClick();
   };
+
+  const getPlacement = () => {
+    return placement || globalParams.placement || 'topLeft';
+  };
+  const getPosition = (type: 'top' | 'bottom') => {
+    const position = getPlacement();
+    if (position === `${type}Left` || position === `${type}Right`) {
+      return props[type] || globalParams[type] || '24px';
+    } else {
+      return '';
+    }
+  };
+
   return (
-    <div
-      className={classNames(
-        'mzl_demo_notification',
-        `mzl_demo_notification_${placement}`,
-        `${className}`
-      )}
-      style={{
-        top:
-          placement === 'topLeft' || placement === 'topRight' ? `${top}px` : '',
-        bottom:
-          placement === 'bottomLeft' || placement === 'bottomRight'
-            ? `${bottom}px`
-            : '',
-        ...style,
-      }}
-    >
-      {icon && <span className="mzl_notification_icon">{icon}</span>}
-      {!icon && type && (
-        <span className="mzl_notification_icon">
-          <i className={classNames(`${icons[type]}`)} />
-        </span>
-      )}
-      <div className="mzl_notification_content" onClick={onClickNotification}>
-        <div className="mzl_notification_title_wrapper">
-          <span className="mzl_notification_title">{message}</span>
-          <span
-            onClick={(e) => onCloseNotification(e)}
-            className="mzl_notification_close"
+    <>
+      {isGlobal && <></>}
+      {!isGlobal && (
+        <div
+          className={classNames(
+            'mzl_demo_notification',
+            `mzl_demo_notification_${getPlacement()}`,
+            `${className}`
+          )}
+          style={{
+            top: getPosition('top'),
+            bottom: getPosition('bottom'),
+            ...style,
+          }}
+        >
+          {icon && <span className="mzl_notification_icon">{icon}</span>}
+          {!icon && type && (
+            <span className="mzl_notification_icon">
+              <i className={classNames(`${icons[type]}`)} />
+            </span>
+          )}
+          <div
+            className="mzl_notification_content"
+            onClick={onClickNotification}
           >
-            {closeIcon}
-          </span>
+            <div className="mzl_notification_title_wrapper">
+              <span className="mzl_notification_title">{message}</span>
+              <span
+                onClick={(e) => onCloseNotification(e)}
+                className="mzl_notification_close"
+              >
+                {closeIcon || globalParams.closeIcon || (
+                  <i className="m-icon-close" />
+                )}
+              </span>
+            </div>
+            <div className="mzl_notification_description">{description}</div>
+            {btn && <div className="mzl_notification_operation">{btn}</div>}
+          </div>
         </div>
-        <div className="mzl_notification_description">{description}</div>
-        {btn && <div className="mzl_notification_operation">{btn}</div>}
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 const notification: notificationProps = {
+  config: (config: globalProps) => {
+    if (document.querySelector('.mzl_notification')) return;
+    document.body.appendChild(el);
+    const root = document.querySelector('.mzl_notification_wrapper');
+    return ReactDOM.createRoot(root as HTMLElement).render(
+      <Notification {...config} isGlobal message="" description="" />
+    );
+  },
   useNotification: () => {
     let api: apiProps = {
       open: () => {},
